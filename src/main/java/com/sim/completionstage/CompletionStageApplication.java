952324241a253b038a8cf62f21018883f679bcf4
tlarbals824 <simkm0824@naver.com>
2023-07-01 17:50:59 +0900

    Initial commit

---
+++ src/main/java/com/sim/completionstage/CompletionStageApplication.java
@@ -0,0 +1,22 @@
+package com.sim.completionstage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class CompletionStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompletionStageApplication.class, args);
+//        log.info("start thenAccept");
+//        CompletionStageThenAcceptExample.thenAccept();
+//        CompletionStageThenAcceptRunningExample.thenAccept();
+//        log.info("start thenAcceptAsync");
+//        CompletionStageThenAcceptExample.thenAcceptAsync();
+//        CompletionStageThenAcceptRunningExample.thenAcceptAsync();
+        CompletionStageThenAcceptAsyncExecutorExample.thenAsync();
+    }
+
+}
